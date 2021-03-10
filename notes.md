0625bf63f1654713154226ba810659d3a5b6f143
cep104 <candicelizabethpeters@gmail.com>
2021-03-09 18:51:54 -0600

    notes added

--- notes.md
+++ notes.md
@@ -1,13 +1,39 @@
 INTRO TO OBJECT ORIENTED PROGRAMMING 
 
-##What is OO Programming? 
+##What is OO Programming?
+ -Encapsulates both data and behavior for a common group of things. 
+
+ -mimic real world situations 
+
 
 ##what does OO look like? 
+-class
+-instantiation  => .new
+-methods/ behavior 
+-attributes/properties => readers/writers
 
 ##Classes 
+-a group of objects that all share the same behaviors 
+- blueprint for the objects we are creating
+- behaviors objects and class can use 
+- customizable 
+- class/end & name of class has to be capitalized 
+end
 
 ##Objects 
+instances of classes 
+every object we create is going to have a unique id 
+everything is an object in ruby 
 
 ##Methods
+behaviors a class could have 
+class methods => perform on class as a whole, behavior that belongs to a class (seeing all of the objects in a class) 
+instance methods => belongs to objects we are creating in the class, called on the instance 
+
 
 ##Variables 
+- local variables = scope is the method that it is defined 
+- instance variables = syntax: 1 @ / scope: class 
+- class variables = syntax: 2 @@ / scope: entire class/ to keep track of all the instances in a class ex. @@all
+- global variable = defined outside of a method/ accessible by entire file / not recommended to use. 
+
--- procedural.rb
+++ procedural.rb
@@ -1,38 +1,21 @@
-#create a person with a name and age 
-#what what would be the best way to do this procedural?
-#method 
+#create a person with a name and an age 
 
-def person 
+person1 = { name: "Max", age: 22} 
 
-end
-#walk me through what making a person hash look like
-#key and value pairs
-#hash 
-person1 = {name: "Steve", age: 22} 
-# -we created the hash and stored the hash inside a variable (these are just labels that return objects)
-#is this an object?
+person2 = ["Sam", age: 22]
 
-#what are primitive objects
-#hash
-#string 
-#integer
-#boolean
-#array
+person3name = "Jason"
 
-# person1 = {name: "Steve", age: 22} remember you can also create a new hash doing hash.new (pay attention to this syntax)
-
-#look at hash ruby doc show how hash is the class and all the things on right side are all the available methods you can use on a hash.
-# what are other ways we could create a person in procedural ruby
-person2 = ["Kellen", 22]
-
-person3name = "Max"
 person3age = 22
+
+objects:
+#string
+#array
+#integers 
+#hashes 
 
 def greeting(person)
     puts "Hi, my name is #{person[:name]}, I am #{person[:age]}"
 end
-# this would only work if this passes in a hash I would need to start putting if or else staements or make another method if we use an array or just setting variables. This is starting to get really messy.
 
 greeting(person1)
-
-# also the greeting method has no relationship to any of these objects
